7b374533914be5f3a4752cc5f910024a4bcbec83
Samiliya123 <samiliya967@gmail.com>
2023-11-14 16:43:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Pet = () => {
+  return React.createElement('div', {},  [
+    React.createElement('h1' , {}, 'AlexA'),
+    React.createElement('h2' , {}, 'Dog'),
+    React.createElement('h3' , {}, 'breed'),
+  ])
 }
 
-export default App;
+
+
+
+
+const App = () => {
+  return React.createElement('div', {}, [ React.createElement('h1', {}, 'Adopt Me!'),
+  React.createElement(Pet),
+  React.createElement(Pet),
+  React.createElement(Pet),
+
+])
+}
+export default App
+
+// const container  = document.getElementById('root')
+// const root =  ReactDOM.createRoot(container)
+// root.render(React.createElement(App))
+
+ 
